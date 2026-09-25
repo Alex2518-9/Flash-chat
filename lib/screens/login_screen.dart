86eb9333a44724132632ae6f4f7f86da6b758be9
Alex2518-9 <terigeralex2000@gmail.com>
2026-09-26 08:47:44 +0900

    feat: add progress bar to registration and login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   // use static instead of final for the id variable to make it accessible without creating an instance of the class
@@ -13,53 +16,83 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  bool showSpinner = false;
+  String email = '';
+  String password = '';
+
+  void loginUser(String email, String password) async {
+    try {
+      final user = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (user != null) {
+        // Navigate to chat screen
+        Navigator.pushNamed(context, ChatScreen.id);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(height: 48.0),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
+              SizedBox(height: 48.0),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
               ),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
+              SizedBox(height: 8.0),
+              TextField(
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
               ),
-            ),
 
-            SizedBox(height: 24.0),
-            RoundedButton(
-              text: 'Log In',
-              color: Colors.lightBlueAccent,
-              onPressed: () {
-                //Implement login functionality.
-              },
-            ),
-          ],
+              SizedBox(height: 24.0),
+              RoundedButton(
+                text: 'Log In',
+                color: Colors.lightBlueAccent,
+                onPressed: () {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  loginUser(email, password);
+                  setState(() {
+                    showSpinner = false;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
