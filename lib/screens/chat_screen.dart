f2d2576f5b4b4e98d9884748ccb9b1244d7e5eaf
Alex2518-9 <terigeralex2000@gmail.com>
2026-09-26 13:14:50 +0900

    refactor: reverse the fetched messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -115,7 +115,7 @@ class MessageStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data!.docs;
+        final messages = snapshot.data!.docs.reversed;
         List<MessageBubble> messageWidgets = [];
         for (var message in messages) {
           final messageText = message.get('text');
